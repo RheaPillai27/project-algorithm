1fb0d95ed5ea1faba810a9d6587e0b07c0a24cce
RheaPillai27 <75987947+RheaPillai27@users.noreply.github.com>
2020-12-22 18:54:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var fixedrect,movingrect;
+
+function setup() {
+  createCanvas(1200,800);
+ fixedrect= createSprite(600, 400, 50, 80);
+ fixedrect.shapeColor="red";
+ fixedrect.debug=true;
+ 
+ movingrect= createSprite(600, 400, 50, 80);
+ movingrect.shapeColor="blue";
+ movingrect.debug=true;
+
+}
+
+function draw() {
+  background(255,255,255);  
+
+  movingrect.x=World.mouseX;
+  movingrect.y=World.mouseY;
+
+  if((movingrect.x-fixedrect.x<fixedrect.width/2+movingrect.width/2)&&
+  (fixedrect.x-movingrect.x<fixedrect.width/2+movingrect.width/2)&&
+  (movingrect.y-fixedrect.y<fixedrect.height/2+movingrect.height/2)&&
+  (fixedrect.y-movingrect.y<fixedrect.height/2+movingrect.height/2)){
+    movingrect.shapeColor="purple";
+    fixedrect.shapeColor="purple";
+  }
+  else{
+    movingrect.shapeColor="blue";
+    fixedrect.shapeColor="red";
+  }
+  drawSprites();
+}
